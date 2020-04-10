e8c8ad94c4e43da1e8a609198bac4724c47007b9
Honai Ueoka <ueoka@honai.me>
2020-04-10 11:11:17 +0900

    put map on grid

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col'
 import ResponsiveEmbed from 'react-bootstrap/ResponsiveEmbed';
 
 const Map = ({ map }) => (
   <Container className="mt-3 p-3 border rounded">
-    <h4 className="mb-3" >発生状況</h4>
-    <ResponsiveEmbed aspectRatio='21by9' >
-      <embed type="text/html" src={map.url} />
-    </ResponsiveEmbed>
-    <p className="text-right text-dark mt-2">提供：{map.source} </p>
+    <Row>
+      <Col md={12} lg={8}>
+        <h4 className="mb-3">発生状況</h4>
+        <ResponsiveEmbed aspectRatio='21by9' >
+          <embed type="text/html" src={map.url} />
+        </ResponsiveEmbed>
+        <p className="text-right text-dark mt-2">提供：{map.source} </p>
+      </Col>
+      <Col>何か入る</Col>
+    </Row>
   </Container>
 );
 
