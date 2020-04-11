dc627a5f82ba3b54890b9f63a79c93646e2de87d
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2020-04-11 16:17:29 +0900

    tweak the map style

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -3,12 +3,12 @@ import Container from 'react-bootstrap/Container';
 import ResponsiveEmbed from 'react-bootstrap/ResponsiveEmbed';
 
 const Map = ({ map }) => (
-  <Container className="mt-3 p-3 border rounded">
-    <h4 className="mb-3" >発生状況</h4>
-    <ResponsiveEmbed aspectRatio='21by9' >
+  <Container className="mt-3 p-2 border rounded">
+    <h5 className="mb-2" >発生状況</h5>
+    <ResponsiveEmbed aspectRatio='21by9' className="rounded">
       <embed type="text/html" src={map.url} />
     </ResponsiveEmbed>
-    <p className="text-right text-dark mt-2">提供：{map.source} </p>
+    <p className="text-right text-dark mb-0"><small>提供：{map.source}</small></p>
   </Container>
 );
 
