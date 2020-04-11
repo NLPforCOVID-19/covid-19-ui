6e588452afb79b4f2e1120afad240a472c69f331
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2020-04-11 16:48:36 +0900

    tweak the footer style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Footer = () => (
-  <footer className="mt-5 p-5 bg-light">
+  <footer className="mt-3 p-3 bg-light">
     <p className="text-muted text-center">
       Copyright &copy; 2020 NLPforCOVID-19. All rights reserved.
     </p>
