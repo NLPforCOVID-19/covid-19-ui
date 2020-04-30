6c59fd68e935222a4b61a1376af67df40bb3f60b
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2020-04-30 12:44:43 +0900

    remove copyright icon

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => (
   <footer className="mt-3 p-3 bg-light">
     <p className="text-muted text-center">
-      Copyright &copy; 2020 NLPforCOVID-19. All rights reserved.
+      Copyright 2020 NLPforCOVID-19. All rights reserved.
     </p>
   </footer>
 );
