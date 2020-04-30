109636b3586c53f21854ef5b5c6eb99349717ea5
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2020-04-30 12:54:46 +0900

    add contact

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,8 @@ import React from "react";
 const Footer = () => (
   <footer className="mt-3 p-3 bg-light">
     <p className="text-muted text-center">
-      Copyright 2020 NLPforCOVID-19. All rights reserved.
+      Copyright 2020 <a href="mailto:nlpforcovid-19@nlp.ist.i.kyoto-u.ac.jp" className="text-info">NLPforCOVID-19</a>.
+      All rights reserved.
     </p>
   </footer>
 );
