0a79a0589a8d08aa9ecbc69567a3c64e53c799d6
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2020-04-30 15:02:02 +0900

    fix copyright

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,7 @@ import React from "react";
 const Footer = () => (
   <footer className="mt-3 p-3 bg-light">
     <p className="text-muted text-center">
-      Copyright 2020 <a href="mailto:nlpforcovid-19@nlp.ist.i.kyoto-u.ac.jp" className="text-info">NLPforCOVID-19</a>.
-      All rights reserved.
+      &copy; 2020 <a href="mailto:nlpforcovid-19@nlp.ist.i.kyoto-u.ac.jp" className="text-info">NLPforCOVID-19</a>.
     </p>
   </footer>
 );
