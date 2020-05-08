4afcd5bc6bfca546feb5ca3851359638bdec5013
Honai Ueoka <ueoka@honai.me>
2020-05-08 15:03:01 +0900

    feat: add meta description

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -12,6 +12,7 @@ const Index = () => {
     <div className="wrapper">
       <Head>
         <title>{meta.title}</title>
+        <meta name="description" content={meta.desc} />
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css?family=Noto+Sans+JP" rel="stylesheet" />
       </Head>
