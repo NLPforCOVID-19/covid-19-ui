5c34a0f12ac55bbd0ffb220d6c3377f3a84b840c
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2020-05-11 12:00:58 +0900

    [WIP] trim titles that exceed 38 characters

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -22,7 +22,6 @@ function EntryIcon({entry}) {
 }
 
 const Page = ({ entry, topic }) => {
-  const isJp = entry.country === 'jp';
   const topicData = entry.topics.find(t => t.name === topic);
   const snippet = topicData ? topicData.snippet : '';
   return (
@@ -30,26 +29,7 @@ const Page = ({ entry, topic }) => {
       <div className="icon"><EntryIcon entry={entry} /></div>
       <div className="news">
         <div className="title-wrap">
-          <a
-            href={isJp ? entry.url : makeTranslatedUrl(entry.url)}
-            target="_blank"
-            rel="noreferrer noopener"
-            className="text-info"
-          >
-            <span className="small date">[{dayjs(entry.orig.timestamp).format('MM/DD')}]</span>
-            {" "}{entry.ja_translated.title}{" "}
-          </a>
-          {isJp || (
-            <>
-              <a
-                href={entry.url}
-                target="_blank"
-                title="元の言語で表示する"
-              >
-                <span className="material-icons open-in-new">open_in_new</span>
-              </a>
-            </>
-          )}
+          <Title entry={entry} />
         </div>
         <Snippet text={snippet} />
       </div>
@@ -61,6 +41,41 @@ const Page = ({ entry, topic }) => {
           flex: 0 0 16px;
           margin-right: 3px;
         }
+      `}</style>
+    </li>
+  );
+};
+
+const Title = ({ entry }) => {
+  const MAX_LEN = 36;
+  const isJp = entry.country === 'jp';
+  const url = isJp ? entry.url : makeTranslatedUrl(entry.url);
+  const day = dayjs(entry.orig.timestamp).format('MM/DD');
+  const full_title = entry.ja_translated.title;
+  const title = full_title.length < MAX_LEN ? full_title : full_title.slice(0, MAX_LEN).concat("...");
+  return (
+    <>
+      <a
+        href={url}
+        target="_blank"
+        rel="noreferrer noopener"
+        className="text-info"
+      >
+        <span className="small date">[{day}]</span>
+        {" "}{title}{" "}
+      </a>
+      {isJp || (
+        <>
+          <a
+            href={entry.url}
+            target="_blank"
+            title="元の言語で表示する"
+          >
+            <span className="material-icons open-in-new">open_in_new</span>
+          </a>
+        </>
+      )}
+      <style jsx>{`
         .material-icons {
           font-size: 1em;
           vertical-align: middle;
@@ -69,8 +84,8 @@ const Page = ({ entry, topic }) => {
           color: rgba(0, 0, 0, 0.5);
         }
       `}</style>
-    </li>
-  );
+    </>
+  )
 };
 
 const Snippet = ({ text }) => <div className="mb-2 small text-secondary">{text}</div>;
