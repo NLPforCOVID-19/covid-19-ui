9f2b2070b32cccb5586749e639424e0fe59b6b56
Honai Ueoka <ueoka@honai.me>
2020-05-12 12:55:04 +0900

    fix link for deploy other than domain root

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   env: {
-    API_URL: process.env.API_URL
+    API_URL: process.env.API_URL,
+    BASE_PATH: process.env.BASE_PATH || ''
   },
   // Pass envs to serve other than domain root.
   assetPrefix: process.env.BASE_PATH || '',
