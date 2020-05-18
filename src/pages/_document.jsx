1495314a1c7b60641f4dbf1ee512c00f5df6c4de
Honai Ueoka <ueoka@honai.me>
2020-05-18 11:40:26 +0900

    add gtag

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,5 +1,10 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
+const gtagScript = `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', '${process.env.GA_TRACKING_ID}');`
+
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx)
@@ -9,7 +14,10 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="ja">
-        <Head />
+        <Head>
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_TRACKING_ID}`} />
+        <script dangerouslySetInnerHTML={{__html: gtagScript}} />
+        </Head>
         <body>
           <Main />
           <NextScript />
