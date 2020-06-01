ed3230af90c9a535c86c353962e723bb4d54ffa8
Honai Ueoka <ueoka@honai.me>
2020-06-01 09:32:41 +0900

    fix about page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -16,25 +16,24 @@ export default () => {
           このサイトは、新型コロナウイルス感染症に関する世界各地域の公式サイトやニュースをクロールし、国・地域×カテゴリで集約、提示しています。
         </p>
         <section>
-          <h4>(1) トップページ</h4>
+          <h4>(1) カテゴリビュー</h4>
           <p className="desc">
-            トップページでは、カテゴリを選んで、世界の国々の情報を一覧することができます。
-            カテゴリには「感染状況」「予防・緊急事態宣言」などがあり、ボタンをクリックすることで切り替えられます。
+            デフォルトでは、上側のタブでカテゴリを選んで、世界の国々の情報を一覧することができます。 カテゴリは「感染状況」「予防・緊急事態宣言」などの6つです。
             <br />
-            カテゴリはまず自動で分類し、順次、人手で検証しています。
+            各記事のカテゴリはまず自動で分類し、順次、人手で検証しています。また、役立つ記事かどうかも人手で判断しています。
           </p>
           <div className="img-wrap">
-            <img src={`${process.env.BASE_PATH}/images/top.png`} width="100%" alt="トップページ" />
+            <img src={`${process.env.BASE_PATH}/images/topic-view.png`} width="100%" alt="トピックビュー" />
           </div>
         </section>
         <section>
-          <h4>(2) 国別のページ</h4>
+          <h4>(2) 地域ビュー</h4>
           <p className="desc">
-            トップページで国名をクリックすると、国別のページに移動します。
-            国別のページでは、その国の全カテゴリの情報を一覧できます。
+            地域名をクリックすると、地域別の表示に切り替わります。この表示では地域ごとに全カテゴリの情報を一覧できます。
+            この画面でカテゴリ名をクリックすると、元のカテゴリビューに戻ります。
           </p>
           <div className="img-wrap">
-            <img src={`${process.env.BASE_PATH}/images/japan.png`} width="100%" alt="日本のページ" />
+            <img src={`${process.env.BASE_PATH}/images/region-view.png`} width="100%" alt="リージョンビュー" />
           </div>
         </section>
       </Container>
