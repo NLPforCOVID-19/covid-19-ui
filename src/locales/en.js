b27a15c685ba3a680833e3b9ab4e69822ad8e1e1
Honai Ueoka <ueoka@honai.me>
2020-08-13 14:36:00 +0900

    add translation file sample

---
+++ src/locales/en.js
@@ -0,0 +1,7 @@
+export const en = {
+  '編集 (管理者向け)': 'Edit (for administrators)',
+  このサイトの使い方: 'How to use this website',
+  '提供:': 'Powered by',
+  公的機関のWebサイトを確認する: 'Check out websites of public institutions',
+  情報源のサイト: 'Information sources list'
+}
---
+++ src/locales/index.js
@@ -0,0 +1,6 @@
+import { ja } from './ja'
+import { en } from './en'
+
+export const translations = { en, ja }
+
+export const localeList = ['en', 'ja']
