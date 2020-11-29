481e09aa08c12d79fe55725c7004f0215462db72
Honai Ueoka <git@honai.me>
2020-11-29 19:43:52 +0900

    add store for ui state

---
+++ src/redux/ui.ts
@@ -0,0 +1,44 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+import { RegionId, Topic } from '@src/types'
+import { fetchMeta } from '@src/redux/asyncActions'
+import { RootState } from '@src/redux/index'
+
+type ViewMode = 'topic' | 'region' | 'neutral'
+
+interface State {
+  viewMode: ViewMode
+  selectedTopic: Topic
+  selectedRegion: RegionId
+}
+
+const initialState: State = { viewMode: 'neutral', selectedTopic: '', selectedRegion: '' }
+
+const uiSlice = createSlice({
+  name: 'ui',
+  initialState,
+  reducers: {
+    changeViewMode: (state, action: PayloadAction<ViewMode>) => {
+      state.viewMode = action.payload
+    },
+    setTopic: (state, action: PayloadAction<Topic>) => {
+      state.selectedTopic = action.payload
+    },
+    setRegion: (state, action: PayloadAction<RegionId>) => {
+      state.selectedRegion = action.payload
+    }
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchMeta.fulfilled, (state, action) => {
+      state.viewMode = 'topic'
+      state.selectedTopic = action.payload.topics[0]
+      state.selectedRegion = action.payload.regions[0].id
+    })
+  }
+})
+
+export const selectViewMode = (state: RootState) => state.ui.viewMode
+export const selectViewTopic = (state: RootState) => state.ui.selectedTopic
+export const selectViewRegion = (state: RootState) => state.ui.selectedRegion
+
+export default uiSlice.reducer
