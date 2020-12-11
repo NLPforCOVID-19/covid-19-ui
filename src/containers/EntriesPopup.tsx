dbde6f8e49e86c092a4fa439bc9e59818d167f1b
Honai Ueoka <git@honai.me>
2020-12-11 17:38:10 +0900

    add coordinates for countries

--- src/containers/EntriesPopup.tsx
+++ src/containers/EntriesPopup.tsx
@@ -14,8 +14,20 @@ interface EntriesPopupProps {
 }
 
 const countryPosition: Record<string, { longitude: number; latitude: number }> = {
-  jp: { longitude: 140, latitude: 36 },
-  fr: { longitude: 2.3522, latitude: 48.8566 }
+  fr: { latitude: 48.8566, longitude: 2.3522 },
+  es: { latitude: 40.4189, longitude: -3.6919 },
+  de: { latitude: 52.5167, longitude: 13.3833 },
+  kr: { latitude: 37.5833, longitude: 127.0 },
+  in: { latitude: 28.7, longitude: 77.2 },
+  np: { latitude: 27.7167, longitude: 85.3667 },
+  my: { latitude: 3.1478, longitude: 101.6953 },
+  sg: { latitude: 1.3, longitude: 103.8 },
+  jp: { latitude: 35.6897, longitude: 139.6922 },
+  cn: { latitude: 39.905, longitude: 116.3914 },
+  us: { latitude: 38.9047, longitude: -77.0163 },
+  br: { latitude: -15.7744, longitude: -48.0773 },
+  au: { latitude: -35.2931, longitude: 149.1269 },
+  za: { latitude: -33.925, longitude: 18.425 }
 }
 
 export const EntriesPopup: React.FC<EntriesPopupProps> = ({ countryId, entryIds }) => {
@@ -37,7 +49,7 @@ export const EntriesPopup: React.FC<EntriesPopupProps> = ({ countryId, entryIds 
   const { latitude, longitude } = countryPosition[countryId]
 
   return (
-    <Popup longitude={longitude} latitude={latitude} closeButton={false}>
+    <Popup longitude={longitude} latitude={latitude} closeButton={false} sortByDepth={true}>
       <ul className="wrap">{entryIds.map(renderEntry)}</ul>
       <style jsx>{`
         .wrap {
