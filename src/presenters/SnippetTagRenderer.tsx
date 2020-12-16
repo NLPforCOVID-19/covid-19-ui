7cc16fcee4072027dda9c6f4c81d0be6c78c6a6d
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2020-12-16 17:47:17 +0900

    update the background color of highlighted texts in search results

--- src/presenters/SnippetTagRenderer.tsx
+++ src/presenters/SnippetTagRenderer.tsx
@@ -1,22 +1,32 @@
 import { TagForSearchSnippet } from '@src/types'
 
 export const SnippetTagRenderer: React.FC<{ tag: TagForSearchSnippet }> = ({ tag }) => {
-  switch (tag.type) {
-    case 'exact-match':
-      return (
-        <mark>
-          {tag.content}
-          <style jsx>{`
+    switch (tag.type) {
+        case 'exact-match':
+            return (
+                <mark>
+                    {tag.content}
+                    <style jsx>{`
             mark {
-              background-color: #ffee70;
+              background-color: #ffbbbb;
             }
           `}</style>
-        </mark>
-      )
-    case 'match':
-      return <mark>{tag.content}</mark>
-    case 'text':
-    default:
-      return <>{tag.content}</>
-  }
+                </mark>
+            )
+        case 'match':
+            return (
+                <mark>
+                    {tag.content}
+                    <style jsx>{`
+              mark {
+                background-color: #ffeeee;
+              }
+            `}</style>
+                </mark>
+            )
+        case 'text':
+        default:
+            return <>{tag.content}</>
+    }
 }
+
