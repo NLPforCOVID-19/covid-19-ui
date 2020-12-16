228e27a1e1894f7a1d1c1024a0dfe22cc423fb21
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2020-12-16 17:56:56 +0900

    format codes

--- src/presenters/SnippetTagRenderer.tsx
+++ src/presenters/SnippetTagRenderer.tsx
@@ -1,32 +1,31 @@
 import { TagForSearchSnippet } from '@src/types'
 
 export const SnippetTagRenderer: React.FC<{ tag: TagForSearchSnippet }> = ({ tag }) => {
-    switch (tag.type) {
-        case 'exact-match':
-            return (
-                <mark>
-                    {tag.content}
-                    <style jsx>{`
+  switch (tag.type) {
+    case 'exact-match':
+      return (
+        <mark>
+          {tag.content}
+          <style jsx>{`
             mark {
               background-color: #ffbbbb;
             }
           `}</style>
-                </mark>
-            )
-        case 'match':
-            return (
-                <mark>
-                    {tag.content}
-                    <style jsx>{`
-              mark {
-                background-color: #ffeeee;
-              }
-            `}</style>
-                </mark>
-            )
-        case 'text':
-        default:
-            return <>{tag.content}</>
-    }
+        </mark>
+      )
+    case 'match':
+      return (
+        <mark>
+          {tag.content}
+          <style jsx>{`
+            mark {
+              background-color: #ffeeee;
+            }
+          `}</style>
+        </mark>
+      )
+    case 'text':
+    default:
+      return <>{tag.content}</>
+  }
 }
-
