23b118905aa23030f81cea4d12d145db7693fc49
Frederic Bergeron <bergeron@nlp.ist.i.kyoto-u.ac.jp>
2021-02-04 18:16:47 +0900

    Remove TopicSearchForm from Twitter view
    We will reuse the TopicSearchForm from the bottom section.

--- src/containers/TwitterViewContainer.tsx
+++ src/containers/TwitterViewContainer.tsx
@@ -1,17 +1,11 @@
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import { TopicSearchForm } from '@src/components/TopicSearchForm'
 import { TwitterCardContainer } from '@src/containers/TwitterCardContainer'
 
 export const TwitterViewContainer = () => {
     return (
         <Container className="mt-2 mb-2">
-            <Row>
-                <Col>
-                    <TopicSearchForm />
-                </Col>
-            </Row>
             <Row>
                 <TwitterCardContainer />
             </Row>
