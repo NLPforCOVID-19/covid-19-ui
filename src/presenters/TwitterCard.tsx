81268bba86dcf373fc28f5484c83b39f35eb2637
Frederic Bergeron <bergeron@nlp.ist.i.kyoto-u.ac.jp>
2021-02-05 11:24:29 +0900

    Complete refactoring the TwitterCard into smaller objects

--- src/presenters/TwitterCard.tsx
+++ src/presenters/TwitterCard.tsx
@@ -2,6 +2,98 @@ import { memo } from 'react'
 import Col from 'react-bootstrap/Col'
 
 
+const TweetCard: React.DC<Props> = memo((props) => {
+    return (
+        <div class="tweet-card mb-2">
+            <article role="tweet-article" class="tweet-article">
+                <TweetHeader name={props.name} username={props.username} verified={props.verified} avatar={props.avatar} />
+                <TweetBody msgorig={props.msgorig} msgtrans={props.msgtrans} />
+                <TweetFooter timestamp={props.timestamp} />
+            </article>
+        </div>
+    )
+})
+
+const TweetHeader: React.DC<Props> = memo((props) => {
+    return (
+        <div class="tweet-header">
+            <TweetAvatar href={props.avatar} />
+            <TweetNames name={props.name} username={props.username} verified={props.verified} />
+            <TweetTwitterLogo />
+        </div>
+    )
+})
+
+const TweetAvatar: React.DC<Props> = memo((props) => {
+    return (
+        <div class="tweet-avatar">
+            <img class="tweet-avatar-img" src={props.href}/>
+        </div>
+    )
+})
+
+const TweetNames: React.DC<Props> = memo((props) => {
+    return (
+        <div class="tweet-names">
+            <TweetName value={props.name} verified={props.verified} />
+            <TweetUsername value={props.username} />
+        </div>
+    )
+})
+
+const TweetVerifBadge: React.DC<Props> = memo((props) => {
+    return (
+        <div class="tweet-twitter-verif-badge">
+            <a href="" class="tweet-twitter-verif-badge-link" target="_blank">
+                <svg viewBox="0 0 24 24" class="tweet-twitter-verif-badge-logo"><g><path d="M22.5 12.5c0-1.58-.875-2.95-2.148-3.6.154-.435.238-.905.238-1.4 0-2.21-1.71-3.998-3.818-3.998-.47 0-.92.084-1.336.25C14.818 2.415 13.51 1.5 12 1.5s-2.816.917-3.437 2.25c-.415-.165-.866-.25-1.336-.25-2.11 0-3.818 1.79-3.818 4 0 .494.083.964.237 1.4-1.272.65-2.147 2.018-2.147 3.6 0 1.495.782 2.798 1.942 3.486-.02.17-.032.34-.032.514 0 2.21 1.708 4 3.818 4 .47 0 .92-.086 1.335-.25.62 1.334 1.926 2.25 3.437 2.25 1.512 0 2.818-.916 3.437-2.25.415.163.865.248 1.336.248 2.11 0 3.818-1.79 3.818-4 0-.174-.012-.344-.033-.513 1.158-.687 1.943-1.99 1.943-3.484zm-6.616-3.334l-4.334 6.5c-.145.217-.382.334-.625.334-.143 0-.288-.04-.416-.126l-.115-.094-2.415-2.415c-.293-.293-.293-.768 0-1.06s.768-.294 1.06 0l1.77 1.767 3.825-5.74c.23-.345.696-.436 1.04-.207.346.23.44.696.21 1.04z"></path></g></svg>
+            </a>
+        </div>
+    )
+})
+
+const TweetName: React.DC<Props> = memo((props) => {
+    return (
+        <div class="tweet-name">
+            <span>{props.value}</span>
+            {props.verified &&
+                <TweetVerifBadge />}
+        </div>
+    )
+})
+
+const TweetUsername: React.DC<Props> = memo((props) => {
+    return (
+        <div class="tweet-username">
+            <span>@{props.value}</span>
+        </div>
+    )
+})
+
+const TweetTwitterLogo: React.DC<Props> = memo((props) => {
+    return (
+        <div class="tweet-twitter-logo">
+            <a href="" class="tweet-twitter-logo-link" target="_blank">
+                <svg viewBox="0 0 24 24" class="tweet-twitter-logo"><g><path d="M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z"></path></g></svg>
+            </a>
+        </div>
+    )
+})
+
+const TweetBody: React.DC<Props> = memo((props) => {
+    return (
+        <div class="tweet-body">
+            <div class="tweet-message-original">
+                <span>{props.msgorig}</span>
+            </div>
+            {props.msgtrans &&
+                <div class="tweet-message-translation">
+                    <span class="tweet-translation-header">Translation</span><br/>
+                    <span>{props.msgtrans}</span>
+                </div>}
+        </div>
+    )
+})
+
 const TweetTimestamp: React.FC<Props> = memo((props) => {
     return (
         <div class="tweet-timestamp">
@@ -58,6 +150,7 @@ export const Tweet = memo(() => {
         {
             id: 1000,
             name: "nlpforcovid-19",
+            verified: true,
             username: "nlpforcovid",
             avatar: "https://pbs.twimg.com/profile_images/1347024085952331778/3oBHXOOn_bigger.jpg",
             contentOrig: "欧州がより多くのワクチンを求めている（ヨーロッパ，経済・福祉政策のニュース，France 24",
@@ -67,6 +160,7 @@ export const Tweet = memo(() => {
         {
             id: 1001,
             name: "HDrachster",
+            verified: false,
             username: "HDrachster",
             avatar: "https://pbs.twimg.com/profile_images/1474064923/Hendrik-Drachsler_klein_bigger.jpg",
             contentOrig: "Head of http://edutec.science cares about advancing educational technology, learning analytics, recommender systems, artificial intelligence & learning science",
@@ -76,6 +170,7 @@ export const Tweet = memo(() => {
         {
             id: 1002,
             name: "NASA",
+            verified: true,
             username: "NASA",
             avatar: "https://pbs.twimg.com/profile_images/1321163587679784960/0ZxKlEKB_normal.jpg",
             contentOrig: "Let's go to Mars!",
@@ -85,6 +180,7 @@ export const Tweet = memo(() => {
         {
             id: 1003,
             name: "松本人志",
+            verified: true,
             username: "matsu_bouzu",
             avatar: "https://pbs.twimg.com/profile_images/1274931784979906562/LBxVQhgd_bigger.jpg",
             contentOrig: "所属事務所：よしもとクリエイティブエージェンシー コンビ名：ダウンタウン 血液型：Ｂ型",
@@ -94,6 +190,7 @@ export const Tweet = memo(() => {
         {
             id: 1004,
             name: "LEARN Analytics Research",
+            verified: true,
             username: "NYU_LEARN",
             avatar: "https://pbs.twimg.com/profile_images/963529222340964355/5ZWgf5Jt_bigger.jpg",
             contentOrig: "Network of teachers + technologists using data to promote effective, engaging + equitable education",
@@ -103,6 +200,7 @@ export const Tweet = memo(() => {
         {
             id: 1005,
             name: "nlpforcovid-19",
+            verified: true,
             username: "nlpforcovid",
             avatar: "https://pbs.twimg.com/profile_images/1347024085952331778/3oBHXOOn_bigger.jpg",
             contentOrig: "介護者のための質の悪いマスク?（ヨーロッパ，その他のニュース，Franceinfo）https://lotus.kuee.kyoto-u.ac.jp/NLPforCOVID-19",
@@ -113,44 +211,9 @@ export const Tweet = memo(() => {
     return (
         <div>
         {data.map((tweetData) => (
-            <div class="tweet-card mb-2">
-                <article role="tweet-article" class="tweet-article">
-                    <div class="tweet-header">
-                        <div class="tweet-avatar">
-                            <img class="tweet-avatar-img" src={tweetData.avatar}/>
-                        </div>
-                        <div class="tweet-names">
-                            <div class="tweet-name">
-                                <span>{tweetData.name}</span>
-                                <div class="tweet-twitter-verif-badge">
-                                    <a href="" class="tweet-twitter-verif-badge-link" target="_blank">
-                                        <svg viewBox="0 0 24 24" class="tweet-twitter-verif-badge-logo"><g><path d="M22.5 12.5c0-1.58-.875-2.95-2.148-3.6.154-.435.238-.905.238-1.4 0-2.21-1.71-3.998-3.818-3.998-.47 0-.92.084-1.336.25C14.818 2.415 13.51 1.5 12 1.5s-2.816.917-3.437 2.25c-.415-.165-.866-.25-1.336-.25-2.11 0-3.818 1.79-3.818 4 0 .494.083.964.237 1.4-1.272.65-2.147 2.018-2.147 3.6 0 1.495.782 2.798 1.942 3.486-.02.17-.032.34-.032.514 0 2.21 1.708 4 3.818 4 .47 0 .92-.086 1.335-.25.62 1.334 1.926 2.25 3.437 2.25 1.512 0 2.818-.916 3.437-2.25.415.163.865.248 1.336.248 2.11 0 3.818-1.79 3.818-4 0-.174-.012-.344-.033-.513 1.158-.687 1.943-1.99 1.943-3.484zm-6.616-3.334l-4.334 6.5c-.145.217-.382.334-.625.334-.143 0-.288-.04-.416-.126l-.115-.094-2.415-2.415c-.293-.293-.293-.768 0-1.06s.768-.294 1.06 0l1.77 1.767 3.825-5.74c.23-.345.696-.436 1.04-.207.346.23.44.696.21 1.04z"></path></g></svg>
-                                    </a>
-                                </div>
-                            </div>
-                            <div class="tweet-username">
-                                <span>@{tweetData.username}</span>
-                            </div>
-                        </div>
-                        <div class="tweet-twitter-logo">
-                            <a href="" class="tweet-twitter-logo-link" target="_blank">
-                                <svg viewBox="0 0 24 24" class="tweet-twitter-logo"><g><path d="M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z"></path></g></svg>
-                            </a>
-                        </div>
-                    </div>
-                    <div class="tweet-body">
-                        <div class="tweet-message-original">
-                            <span>{tweetData.contentOrig}</span>
-                        </div>
-                        {tweetData.contentTrans &&
-                            <div class="tweet-message-translation">
-                                <span class="tweet-translation-header">Translation</span><br/>
-                                <span>{tweetData.contentTrans}</span>
-                            </div>}
-                    </div>
-                    <TweetFooter timestamp={tweetData.timestamp}/>
-                </article>
-            </div>
+            <TweetCard name={tweetData.name} username={tweetData.username} verified={tweetData.verified} avatar={tweetData.avatar}
+                msgorig={tweetData.contentOrig} msgtrans={tweetData.contentTrans}
+                timestamp={tweetData.timestamp} />
         ))}
         </div>
     )
