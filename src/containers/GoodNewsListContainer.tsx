9e49dde21f0f96076d1ac648a559abe71fa8fd44
Frederic Bergeron <bergeron@nlp.ist.i.kyoto-u.ac.jp>
2021-09-30 15:40:16 +0900

    Introduce GoodNewsListContainer

---
+++ src/containers/GoodNewsListContainer.tsx
@@ -0,0 +1,37 @@
+import { memo } from 'react'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+import { RegionId, Topic } from '@src/types'
+
+interface Props {
+  region: RegionId
+  topic: Topic
+}
+
+export const GoodNewsListContainer: React.FC<Props> = memo(({ region, topic }) => {
+  return (
+    <div className="mt-3">
+      <Container className="rounded border wrap">
+        <Row className="mt-2 mb-2">
+          <Col className="col-sm-1">
+            <img src="/images/GoodNewsLogo.svg" width="100%" />
+          </Col>
+          <Col className="col-sm-11"></Col>
+        </Row>
+        <Row className="m-3">
+          [09/29]
+          庭駅ナサ場対先メツロモ中化亮影トぞ多防ヘ陣社どぽれご岡世にト栄事都いどゆそ北空シ府升忍曇椎ほふスち。Yahoo!
+          <br />
+          [09/29]
+          米ね開心レロエソ格数ナヘ壮5日トメ政声ちこ的無笑ぎいべ険能フヤ死難あや需歩マ美盗婦きび第票ユスネ親惑償そ。中国政府
+          <br />
+          [09/27] 用残ヤカハ季9括クス載欲ト初再資一スぐ利8更質カナヱマ性配イ景買非こえ。中国政府
+          <br />
+        </Row>
+      </Container>
+    </div>
+  )
+})
+GoodNewsListContainer.displayName = 'GoodNewsListContainer'
--- src/containers/GoodNewsViewContainer.tsx
+++ src/containers/GoodNewsViewContainer.tsx
@@ -1,30 +1,28 @@
-import { memo } from 'react'
+import { useSelector } from 'react-redux'
 import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
 
-export const GoodNewsViewContainer = memo(() => {
-  return (
-    <div className="mt-3">
-      <Container className="rounded border wrap">
-        <Row className="mt-2 mb-2">
-          <Col className="col-sm-1">
-            <img src="/images/GoodNewsLogo.svg" width="100%" />
-          </Col>
-          <Col className="col-sm-11"></Col>
-        </Row>
-        <Row className="m-3">
-          [09/29]
-          庭駅ナサ場対先メツロモ中化亮影トぞ多防ヘ陣社どぽれご岡世にト栄事都いどゆそ北空シ府升忍曇椎ほふスち。Yahoo!
-          <br />
-          [09/29]
-          米ね開心レロエソ格数ナヘ壮5日トメ政声ちこ的無笑ぎいべ険能フヤ死難あや需歩マ美盗婦きび第票ユスネ親惑償そ。中国政府
-          <br />
-          [09/27] 用残ヤカハ季9括クス載欲ト初再資一スぐ利8更質カナヱマ性配イ景買非こえ。中国政府
-          <br />
-        </Row>
-      </Container>
-    </div>
-  )
-})
+import { selectActive } from '@src/redux/regionsTopics'
+import { selectViewMode } from '@src/redux/ui'
+import { Loading } from '@src/components/Loading'
+import { GoodNewsListContainer } from '@src/containers/GoodNewsListContainer'
+
+export const GoodNewsViewContainer = () => {
+  const { region: activeRegion } = useSelector(selectActive)
+  const viewMode = useSelector(selectViewMode)
+
+  switch (viewMode) {
+    case 'region':
+      return <GoodNewsListContainer region={activeRegion} topic="all" />
+
+    case 'topic':
+      return <GoodNewsListContainer region="all" topic="all" />
+
+    default:
+      return (
+        <Container className="text-center">
+          <Loading />
+        </Container>
+      )
+  }
+}
 GoodNewsViewContainer.displayName = 'GoodNewsViewContainer'
