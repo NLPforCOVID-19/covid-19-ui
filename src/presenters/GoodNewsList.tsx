2ee54ae21d7e9644aa8d3328f87ad938c48d30fd
Frederic Bergeron <bergeron@nlp.ist.i.kyoto-u.ac.jp>
2021-10-04 14:34:57 +0900

    Add styles for good news entries

--- src/presenters/GoodNewsList.tsx
+++ src/presenters/GoodNewsList.tsx
@@ -7,22 +7,56 @@ export const GoodNewsList = () => {
       <Col>
         <div className="wrap">
           <div className="scroll">
-            [09/29]
-            庭駅ナサ場対先メツロモ中化亮影トぞ多防ヘ陣社どぽれご岡世にト栄事都いどゆそ北空シ府升忍曇椎ほふスち。Yahoo!
-            <br />
-            [09/29]
-            米ね開心レロエソ格数ナヘ壮5日トメ政声ちこ的無笑ぎいべ険能フヤ死難あや需歩マ美盗婦きび第票ユスネ親惑償そ。中国政府
-            <br />
-            [09/27] 用残ヤカハ季9括クス載欲ト初再資一スぐ利8更質カナヱマ性配イ景買非こえ。中国政府
-            <br />
-            [09/29]
-            庭駅ナサ場対先メツロモ中化亮影トぞ多防ヘ陣社どぽれご岡世にト栄事都いどゆそ北空シ府升忍曇椎ほふスち。Yahoo!
-            <br />
-            [09/29]
-            米ね開心レロエソ格数ナヘ壮5日トメ政声ちこ的無笑ぎいべ険能フヤ死難あや需歩マ美盗婦きび第票ユスネ親惑償そ。中国政府
-            <br />
-            [09/27] 用残ヤカハ季9括クス載欲ト初再資一スぐ利8更質カナヱマ性配イ景買非こえ。中国政府
-            <br />
+            <div className="news">
+                <div className="title text-info">
+                    <span className="small text-muted">[10/29]</span>
+                    <span className="small text-muted">&thinsp;(日本)</span>
+                    <a href="" target="_blank" rel="noreferrer">&thinsp;庭駅ナサ場対先メツロモ中化亮影トぞ多防ヘ陣社どぽれご岡世にト栄事都いどゆそ北空シ府升忍曇椎ほふスち。</a>
+                <span className="source small">
+                    <a href="" target="_blank" rel="noreferrer">&thinsp;Yahoo!</a>
+                </span>
+                </div>
+            </div>
+            <div className="news">
+                <div className="title text-info">
+                    <span className="small text-muted">[10/29]</span>
+                    <span className="small text-muted">&thinsp;(中国)</span>
+                    <a href="" target="_blank" rel="noreferrer">&thinsp;米ね開心レロエソ格数ナヘ壮5日トメ政声ちこ的無笑ぎいべ険能フヤ死難あや需歩マ美盗婦きび第票ユスネ親惑償そ。</a>
+                <span className="source small">
+                    <a href="" target="_blank" rel="noreferrer">&thinsp;中国政府</a>
+                </span>
+                </div>
+            </div>
+            <div className="news">
+                <div className="title text-info">
+                    <span className="small text-muted">[10/29]</span>
+                    <span className="small text-muted">&thinsp;(中国)</span>
+                    <a href="" target="_blank" rel="noreferrer">&thinsp;用残ヤカハ季9括クス載欲ト初再資一スぐ利8更質カナヱマ性配イ景買非こえ。</a>
+                <span className="source small">
+                    <a href="" target="_blank" rel="noreferrer">&thinsp;中国政府</a>
+                </span>
+                </div>
+            </div>
+            <div className="news">
+                <div className="title text-info">
+                    <span className="small text-muted">[10/29]</span>
+                    <span className="small text-muted">&thinsp;(日本)</span>
+                    <a href="" target="_blank" rel="noreferrer">&thinsp;庭駅ナサ場対先メツロモ中化亮影トぞ多防ヘ陣社どぽれご岡世にト栄事都いどゆそ北空シ府升忍曇椎ほふスち。</a>
+                <span className="source small">
+                    <a href="" target="_blank" rel="noreferrer">&thinsp;Yahoo!</a>
+                </span>
+                </div>
+            </div>
+            <div className="news">
+                <div className="title text-info">
+                    <span className="small text-muted">[10/29]</span>
+                    <span className="small text-muted">&thinsp;(中国)</span>
+                    <a href="" target="_blank" rel="noreferrer">&thinsp;米ね開心レロエソ格数ナヘ壮5日トメ政声ちこ的無笑ぎいべ険能フヤ死難あや需歩マ美盗婦きび第票ユスネ親惑償そ。</a>
+                <span className="source small">
+                    <a href="" target="_blank" rel="noreferrer">&thinsp;中国政府</a>
+                </span>
+                </div>
+            </div>
             <div className="scroll-observe" />
           </div>
         </div>
