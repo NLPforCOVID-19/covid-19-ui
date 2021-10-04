e5ad502f06dc7e2587ea2de61852d4d4912a50e0
Frederic Bergeron <bergeron@nlp.ist.i.kyoto-u.ac.jp>
2021-10-04 16:45:04 +0900

    Fix format and lint issues

--- src/presenters/GoodNewsEntryView.tsx
+++ src/presenters/GoodNewsEntryView.tsx
@@ -17,40 +17,57 @@ interface Props {
 
 export const GoodNewsEntryView: React.FC<Props> = memo((props) => {
   const { t } = useTranslation()
-  const { title, mainUrl, date, sourceName, sourceUrl, renderSnippet, altUrl, mark, country, renderIcon } = props
+  const { title, mainUrl, date, sourceName, sourceUrl, altUrl, mark, country, renderIcon } = props
   return (
+    <div>
+      <div className="icon">{renderIcon()}</div>
       <div className="news">
-            <div className="title text-info">
-                <span className="small text-muted">[{date}]</span>
-                <span className="small text-muted">&thinsp;({country})</span>
-                <a href="{mainUrl}" target="_blank" rel="noreferrer">&thinsp;{title}</a>
-                {altUrl && (
-                    <span>
-                        <a href="{altUrl}" target="_blank" rel="noreferrer" title={t('元の言語で表示')}>&thinsp;<Icons.OpenInNew /></a>
-                    </span>
-                )}
-                <span className="source small">
-                    <a href="{sourceUrl}" target="_blank" rel="noreferrer">&thinsp;{sourceName}</a>
-                </span>
-            </div>
-          <style jsx>{`
-              .wrap {
-                height: 100px;
-                display: flex;
-                flex-flow: column nowrap;
-              }
-              .header {
-                font-size: 1.3rem;
-              }
-              .scroll {
-                overflow-y: auto;
-              }
-              .scroll-observe {
-                height: 1px;
-              }
-          `}</style>
+        <div className="title text-info">
+          <span className="small text-muted">[{date}]</span>
+          {mark && (
+            <span>
+              &thinsp;<mark className="small text-muted">{mark}</mark>
+            </span>
+          )}
+          <span className="small text-muted">&thinsp;({country})</span>
+          <a href={mainUrl} target="_blank" rel="noreferrer">
+            &thinsp;{title}
+          </a>
+          {altUrl && (
+            <span>
+              <a href="{altUrl}" target="_blank" rel="noreferrer" title={t('元の言語で表示')}>
+                &thinsp;
+                <Icons.OpenInNew />
+              </a>
+            </span>
+          )}
+          <span className="source small">
+            <a href={sourceUrl} target="_blank" rel="noreferrer">
+              &thinsp;{sourceName}
+            </a>
+          </span>
+        </div>
       </div>
+      <style jsx>{`
+        .wrap {
+          height: 100px;
+          display: flex;
+          flex-flow: column nowrap;
+        }
+        .header {
+          font-size: 1.3rem;
+        }
+        .scroll {
+          overflow-y: auto;
+        }
+        .scroll-observe {
+          height: 1px;
+        }
+        mark {
+          background-color: #ffee70;
+        }
+      `}</style>
+    </div>
   )
 })
 GoodNewsEntryView.displayName = 'GoodNewsEntryView'
-
--- src/presenters/GoodNewsList.tsx
+++ src/presenters/GoodNewsList.tsx
@@ -1,19 +1,95 @@
+import { useCallback } from 'react'
+import { useSelector } from 'react-redux'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import * as Icons from '@src/components/Icons'
+
+import { selectEditMode } from '@src/redux/ui'
+import * as Icon from '@src/components/Icons'
 import { GoodNewsEntryView } from '@src/presenters/GoodNewsEntryView'
 
 export const GoodNewsList = () => {
+  const editMode = useSelector(selectEditMode)
+
+  const handleClickEdit = useCallback(
+    (e: React.MouseEvent) => {
+      e.preventDefault()
+      if (!editMode) return
+      return
+    },
+    [editMode]
+  )
+
+  const renderIcon = useCallback(() => {
+    return (
+      <>
+        {editMode && (
+          <a href="#" onClick={handleClickEdit}>
+            <Icon.Edit />
+          </a>
+        )}
+      </>
+    )
+  }, [editMode, handleClickEdit])
   return (
     <Row className="mt-2 mb-2">
       <Col>
         <div className="wrap">
           <div className="scroll">
-            <GoodNewsEntryView title="庭駅ナサ場対先メツロモ中化亮影トぞ多防ヘ陣社どぽれご岡世にト栄事都いどゆそ北空シ府升忍曇椎ほふスち。" mainUrl="" altUrl="" date="10/29" sourceName="Yahoo!" sourceUrl="" country="日本" renderIcon="" mark=""/>
-            <GoodNewsEntryView title="米ね開心レロエソ格数ナヘ壮5日トメ政声ちこ的無笑ぎいべ険能フヤ死難あや需歩マ美盗婦きび第票ユスネ親惑償そ。" mainUrl="" altUrl="test" date="10/29" sourceName="中国政府" sourceUrl="" country="中国" renderIcon="" mark=""/>
-            <GoodNewsEntryView title="用残ヤカハ季9括クス載欲ト初再資一スぐ利8更質カナヱマ性配イ景買非こえ。" mainUrl="" altUrl="test" date="10/29" sourceName="中国政府" sourceUrl="" country="中国" renderIcon="" mark=""/>
-            <GoodNewsEntryView title="庭駅ナサ場対先メツロモ中化亮影トぞ多防ヘ陣社どぽれご岡世にト栄事都いどゆそ北空シ府升忍曇椎ほふスち。" mainUrl="" altUrl="test" date="10/29" sourceName="Yahoo!" sourceUrl="" country="日本" renderIcon="" mark=""/>
-            <GoodNewsEntryView title="米ね開心レロエソ格数ナヘ壮5日トメ政声ちこ的無笑ぎいべ険能フヤ死難あや需歩マ美盗婦きび第票ユスネ親惑償そ。" mainUrl="" altUrl="test" date="10/29" sourceName="中国政府" sourceUrl="" country="中国" renderIcon="" mark=""/>
+            <GoodNewsEntryView
+              title="庭駅ナサ場対先メツロモ中化亮影トぞ多防ヘ陣社どぽれご岡世にト栄事都いどゆそ北空シ府升忍曇椎ほふスち。"
+              mainUrl="https://www.example.com"
+              altUrl=""
+              date="10/29"
+              sourceName="Yahoo!"
+              sourceUrl=""
+              country="日本"
+              renderIcon={renderIcon}
+              mark=""
+            />
+            <GoodNewsEntryView
+              title="米ね開心レロエソ格数ナヘ壮5日トメ政声ちこ的無笑ぎいべ険能フヤ死難あや需歩マ美盗婦きび第票ユスネ親惑償そ。"
+              mainUrl=""
+              altUrl="test"
+              date="10/29"
+              sourceName="中国政府"
+              sourceUrl=""
+              country="中国"
+              renderIcon={renderIcon}
+              mark=""
+            />
+            <GoodNewsEntryView
+              title="用残ヤカハ季9括クス載欲ト初再資一スぐ利8更質カナヱマ性配イ景買非こえ。"
+              mainUrl=""
+              altUrl="test"
+              date="10/29"
+              sourceName="中国政府"
+              sourceUrl=""
+              country="中国"
+              renderIcon={renderIcon}
+              mark=""
+            />
+            <GoodNewsEntryView
+              title="庭駅ナサ場対先メツロモ中化亮影トぞ多防ヘ陣社どぽれご岡世にト栄事都いどゆそ北空シ府升忍曇椎ほふスち。"
+              mainUrl=""
+              altUrl="test"
+              date="10/29"
+              sourceName="Yahoo!"
+              sourceUrl=""
+              country="日本"
+              renderIcon={renderIcon}
+              mark=""
+            />
+            <GoodNewsEntryView
+              title="米ね開心レロエソ格数ナヘ壮5日トメ政声ちこ的無笑ぎいべ険能フヤ死難あや需歩マ美盗婦きび第票ユスネ親惑償そ。"
+              mainUrl=""
+              altUrl="test"
+              date="10/29"
+              sourceName="中国政府"
+              sourceUrl=""
+              country="中国"
+              renderIcon={renderIcon}
+              mark=""
+            />
             <div className="scroll-observe" />
           </div>
         </div>
