54f430da94ecf3d397c86ff0382618bb2df56ffd
Frederic Bergeron <bergeron@nlp.ist.i.kyoto-u.ac.jp>
2021-10-04 14:49:19 +0900

    Add icon to open original article

--- src/presenters/GoodNewsList.tsx
+++ src/presenters/GoodNewsList.tsx
@@ -1,5 +1,6 @@
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import * as Icons from '@src/components/Icons'
 
 export const GoodNewsList = () => {
   return (
@@ -12,9 +13,9 @@ export const GoodNewsList = () => {
                     <span className="small text-muted">[10/29]</span>
                     <span className="small text-muted">&thinsp;(日本)</span>
                     <a href="" target="_blank" rel="noreferrer">&thinsp;庭駅ナサ場対先メツロモ中化亮影トぞ多防ヘ陣社どぽれご岡世にト栄事都いどゆそ北空シ府升忍曇椎ほふスち。</a>
-                <span className="source small">
-                    <a href="" target="_blank" rel="noreferrer">&thinsp;Yahoo!</a>
-                </span>
+                    <span className="source small">
+                        <a href="" target="_blank" rel="noreferrer">&thinsp;Yahoo!</a>
+                    </span>
                 </div>
             </div>
             <div className="news">
@@ -22,9 +23,12 @@ export const GoodNewsList = () => {
                     <span className="small text-muted">[10/29]</span>
                     <span className="small text-muted">&thinsp;(中国)</span>
                     <a href="" target="_blank" rel="noreferrer">&thinsp;米ね開心レロエソ格数ナヘ壮5日トメ政声ちこ的無笑ぎいべ険能フヤ死難あや需歩マ美盗婦きび第票ユスネ親惑償そ。</a>
-                <span className="source small">
-                    <a href="" target="_blank" rel="noreferrer">&thinsp;中国政府</a>
-                </span>
+                    <span>
+                        <a href="" target="_blank" rel="noreferrer" title="">&thinsp;<Icons.OpenInNew /></a>
+                    </span>
+                    <span className="source small">
+                        <a href="" target="_blank" rel="noreferrer">&thinsp;中国政府</a>
+                    </span>
                 </div>
             </div>
             <div className="news">
@@ -32,9 +36,12 @@ export const GoodNewsList = () => {
                     <span className="small text-muted">[10/29]</span>
                     <span className="small text-muted">&thinsp;(中国)</span>
                     <a href="" target="_blank" rel="noreferrer">&thinsp;用残ヤカハ季9括クス載欲ト初再資一スぐ利8更質カナヱマ性配イ景買非こえ。</a>
-                <span className="source small">
-                    <a href="" target="_blank" rel="noreferrer">&thinsp;中国政府</a>
-                </span>
+                    <span>
+                        <a href="" target="_blank" rel="noreferrer" title="">&thinsp;<Icons.OpenInNew /></a>
+                    </span>
+                    <span className="source small">
+                        <a href="" target="_blank" rel="noreferrer">&thinsp;中国政府</a>
+                    </span>
                 </div>
             </div>
             <div className="news">
@@ -42,9 +49,9 @@ export const GoodNewsList = () => {
                     <span className="small text-muted">[10/29]</span>
                     <span className="small text-muted">&thinsp;(日本)</span>
                     <a href="" target="_blank" rel="noreferrer">&thinsp;庭駅ナサ場対先メツロモ中化亮影トぞ多防ヘ陣社どぽれご岡世にト栄事都いどゆそ北空シ府升忍曇椎ほふスち。</a>
-                <span className="source small">
-                    <a href="" target="_blank" rel="noreferrer">&thinsp;Yahoo!</a>
-                </span>
+                    <span className="source small">
+                        <a href="" target="_blank" rel="noreferrer">&thinsp;Yahoo!</a>
+                    </span>
                 </div>
             </div>
             <div className="news">
@@ -52,9 +59,12 @@ export const GoodNewsList = () => {
                     <span className="small text-muted">[10/29]</span>
                     <span className="small text-muted">&thinsp;(中国)</span>
                     <a href="" target="_blank" rel="noreferrer">&thinsp;米ね開心レロエソ格数ナヘ壮5日トメ政声ちこ的無笑ぎいべ険能フヤ死難あや需歩マ美盗婦きび第票ユスネ親惑償そ。</a>
-                <span className="source small">
-                    <a href="" target="_blank" rel="noreferrer">&thinsp;中国政府</a>
-                </span>
+                    <span>
+                        <a href="" target="_blank" rel="noreferrer" title="">&thinsp;<Icons.OpenInNew /></a>
+                    </span>
+                    <span className="source small">
+                        <a href="" target="_blank" rel="noreferrer">&thinsp;中国政府</a>
+                    </span>
                 </div>
             </div>
             <div className="scroll-observe" />
